3ec1f09812d9896e9b11383cf16408044a63cd53
Moradel Henri <moradelhenri@gmail.com>
2021-11-11 06:16:12 +0100

    recmmence autre methode

--- front/index.js
+++ front/index.js
@@ -1,6 +1,9 @@
+// variable pour tableaux
+
 let kanapData = [];
 const fetchkanap = async () => {
   // Récupérer les articles depuis l'API
+
 
   await fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
@@ -12,22 +15,44 @@ const fetchkanap = async () => {
 const kanapDisplay = async () => {
   await fetchkanap();
   document.getElementById("items").innerHTML = kanapData.map(
-    (kanap) => ` 
-     
-    <a  id="cart${kanap._id}">
- 
-   
+    (kanap) =>
+    
+      `       
+    <a  id="product${kanap._id}">
+  
     <article>
        
     <img src="${kanap.imageUrl}" alt="image de canapé${kanap.name}" />
         <h3 class="titre product">${kanap.name}</h3>
+        <button id =${kanap._id} class=boutonProduit>voir</button>
         <p class="productDescription">${kanap.altTxt}
          
         </p>
+       
       </article>
-           </a>
-   
-    `
+      </a>
+       
+  `
+    ,
+  )
+  .join("");
+ 
+  let boutons = document.querySelectorAll(".boutonProduit")
+  console.log(boutons);
+  
+  boutons.forEach((bouton) =>
+    bouton.addEventListener("click", () => {
+      console.log(bouton);
+      window.location = 'product.html?${bouton.id}';
+    }),
+
   );
-};
+  };
+
 kanapDisplay();
+
+
+const URLSearchParams=new URLSearchParams("bouton.id")
+
+const id = URLSearchParams.length("id");
+console.log(id);
---
+++ front/product.js
@@ -0,0 +1,8 @@
+
+
+let params = new URLSearchParams(Window.location.Search)
+
+console.log( params);
+' <img src="../images/logo.png" alt="Photographie d'un canapé"> -->
+
+'
