212a91dfc366580c97c8116def4e29a46d3cf881
Moradel Henri <moradelhenri@gmail.com>
2021-11-22 10:24:02 +0100

    integration id bouton et produit

--- front/index.js
+++ front/index.js
@@ -1,9 +1,6 @@
-// variable pour tableaux
-
 let kanapData = [];
 const fetchkanap = async () => {
   // Récupérer les articles depuis l'API
-
 
   await fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
@@ -14,45 +11,34 @@ const fetchkanap = async () => {
 };
 const kanapDisplay = async () => {
   await fetchkanap();
-  document.getElementById("items").innerHTML = kanapData.map(
-    (kanap) =>
+  document.getElementById("items").innerHTML = kanapData
+    .map(
+      (kanap) =>
+        ` 
+    <a id="product${kanap._id}" >
+      
+                 <article>
+              <img src="${kanap.imageUrl}" alt=" image de canapé" ${kanap.name}/>
+              <h3 class="productName">${kanap.name}</h3>
+              <button id= "${kanap._id}"class= "bouton-produits ">voir </button>
+              <p class="productDescription">${kanap.altTxt}</p>
+            </article>    
     
-      `       
-    <a  id="product${kanap._id}">
-  
-    <article>
-       
-    <img src="${kanap.imageUrl}" alt="image de canapé${kanap.name}" />
-        <h3 class="titre product">${kanap.name}</h3>
-        <button id =${kanap._id} class=boutonProduit>voir</button>
-        <p class="productDescription">${kanap.altTxt}
-         
-        </p>
-       
-      </article>
-      </a>
-       
-  `
-    ,
-  )
-  .join("");
- 
-  let boutons = document.querySelectorAll(".boutonProduit")
+            </a>
+   
+            `
+    )
+
+    .join("");
+
+  let boutons = document.querySelectorAll(".bouton-produits");
   console.log(boutons);
-  
   boutons.forEach((bouton) =>
     bouton.addEventListener("click", () => {
       console.log(bouton);
-      window.location = 'product.html?${bouton.id}';
-    }),
-
+      window.location = `product.html?${bouton.id}`; 
+    })
   );
-  };
+};
 
 kanapDisplay();
-
-
-const URLSearchParams=new URLSearchParams("bouton.id")
-
-const id = URLSearchParams.length("id");
-console.log(id);
--- front/product.js
+++ front/product.js
@@ -1,8 +1,23 @@
+// récuperation de L'id avec la méthode window location search
+
+const produit = window.location.search.split("?").join("");
+
+// récuperation  d'un seul article  dans produitdata!
+
+let produitData = [];
+// appel de l'article grace à fetch et la fonction fetchProduit
+const fetchProduit = async () => {
+  await fetch(`http://localhost:3000/api/products/${produit}`)
+    .then((res) => res.json())
+    .then((promise) => {
+      console.log((produitData=promise));
+    });
+  
+
+};
+produitData
+console.log(produitData);
+
+fetchProduit()
 
 
-let params = new URLSearchParams(Window.location.Search)
-
-console.log( params);
-' <img src="../images/logo.png" alt="Photographie d'un canapé"> -->
-
-'
